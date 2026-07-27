dd6cb0e20ff881c696ce34e4236222ba6ed6194e
harkanni <harkanzyempire001@gmail.com>
2026-07-27 20:05:44 +0100

    infra-workload

---
+++ infra/main.tf
@@ -0,0 +1,13 @@
+module "website" {
+  source = "./modules/static-site"
+  
+  bucket_name = var.bucket_name
+}
+
+module "oidc" {
+  source = "./modules/oidc-manager"
+
+  repositories = var.github_repositories
+
+  role_name = "GitHubActionsRole"
+}
