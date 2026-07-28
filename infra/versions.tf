0e2708aebce068edc96223b75e5cc0b6b5e70107
harkanni <harkanzyempire001@gmail.com>
2026-07-28 14:41:02 +0100

    authentications

--- infra/versions.tf
+++ infra/versions.tf
@@ -5,4 +5,12 @@ terraform {
       version = "~>6.0"
     }
   }
+
+  backend "s3" {
+    bucket       = "whatsapp-state-file-bucket"
+    key          = "state/terraform.tfstate"
+    region       = "us-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
